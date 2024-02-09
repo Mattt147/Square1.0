7c2dd6882e7efafd4c00a8b1441ae346e767a17c
nstsss <nastastrumanowa@gmail.com>
2024-02-09 12:17:02 +0300

    сдж

--- ConsoleInterface/Program.cs
+++ ConsoleInterface/Program.cs
@@ -10,7 +10,62 @@ namespace ConsoleInterface
     {
         static void Main(string[] args)
         {
+            bool menu = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Ограда фермера: ");
+                    Console.WriteLine("1. Круг");
+                    Console.WriteLine("2. Квадрат");
+                    Console.WriteLine("3. Прямоугольник_2");
+                    Console.WriteLine("4. Прямоугольник_Ф");
+                    Console.WriteLine("5. Трегольник");
+                    Console.WriteLine("6. Шестиугольник");
+                    Console.WriteLine("7. Ромб");
+                    Console.WriteLine("8. Выход из программы");
+                    Console.WriteLine("Выберте форму участка: ");
+                    int num = int.Parse(Console.ReadLine());
+                    switch (num)
+                    {
+                        case 1:
 
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+
+                            break;
+                        case 6:
+
+                            break;
+                        case 7:
+
+                            break;
+                        case 8:
+
+                            break;
+
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Вы ввели не число");
+                    continue;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            } while (menu);
         }
     }
 }
