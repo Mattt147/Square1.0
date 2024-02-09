fa789cf9514ad78c07ec8f96eeda47977793384a
Asus647 <159523325+Asus647@users.noreply.github.com>
2024-02-09 14:31:49 +0300

    bambam(нужно дописать 4 теста Симону) 4 теста готовы

--- TestSquare/Program.cs
+++ TestSquare/Program.cs
@@ -14,13 +14,16 @@ namespace TestSquare
             Console.WriteLine("Задача Ограда фермера");
             CommonTest();
             CircleTest();
-            QudratTest();
+            QuadrateTest();
             Rectangle2Test();
             RectangleGoldTest();
             TriangleTest();
             HexagonTest();
             RombTest();
         }
+        // нужно дописать последние 4 теста
+        //это тебе на тебе, Симон
+        //надеемся на тебя
         static void CommonTest()
         {
             double perimetr = 55;
@@ -39,35 +42,66 @@ namespace TestSquare
             Console.WriteLine("Площадь треугольника = " + SquareTriangle);
             Console.WriteLine("Площадь шестиугольника = " + SquareHexagon);
             Console.WriteLine("Площадь ромба = " + SquareRomb);
+            Console.WriteLine();
         }
         static void CircleTest()
         {
             Console.WriteLine("Тест Circle");
-            Console.WriteLine("perimetr - целое, равное 5. Площадь ");
-        }
-        static void QudratTest()
-        {
+            Console.WriteLine("perimetr - целое, равное 5. Площадь намного меньше, но чуть больше половины Пи.");
+            Console.WriteLine("Площадь = " + SquareModel.SquareCircle(5));
+            
+            Console.WriteLine("perimetr - вещественное, равное 2*Пи. Результат практически совпадает с Пи");
+            Console.WriteLine("Площадь = " + SquareModel.SquareCircle(2*Math.PI));
 
+            Console.WriteLine("perimetr - вещественное, меньше 1, равное 0.5");
+            Console.WriteLine("Площадь в сто раз меньше результата при perimetr, равном 5");
+            Console.WriteLine("Площадь = " + SquareModel.SquareCircle(0.5));
+            Console.WriteLine();
+        }
+        static void QuadrateTest()
+        {
+            Console.WriteLine("Тест Quadrate");
+            Console.WriteLine("perimetr - целое, равное 15. Площадь меньше почти в 14 раз, а стороны(-а) -");
+            Console.WriteLine("вещественные числа(-о).");
+            Console.WriteLine("Площадь = " + SquareModel.SquareQudrat(15));
+
+            Console.WriteLine("perimetr - вещественное, равное 3*Пи. Площадь почти в 2 раза больше Пи");
+            Console.WriteLine("Площадь = " + SquareModel.SquareQudrat(3*Math.PI));
+
+            Console.WriteLine("perimetr - вещественное, меньше 1, равное 0.29. Площадь почти в тысячу раз меньше,");
+            Console.WriteLine("чем площадь при perimetr, равном 3*Пи");
+            Console.WriteLine("Площадь = " + SquareModel.SquareQudrat(0.29));
+            Console.WriteLine();
         }
         static void Rectangle2Test()
         {
+            Console.WriteLine("Тест Rectangle2Test");
+            Console.WriteLine("perimetr - целое, равное 27. Площадь меньше почти в 4 раза, а стороны - числа вещественные.");
+            Console.WriteLine("Площадь = " + SquareModel.SquareRectangle2(27));
 
+            Console.WriteLine("perimetr - вещественное, равное 1.25*Пи. Площадь меньше 1, значит одна из сторон тоже меньше 1,");
+            Console.WriteLine("либо обе стороны меньше 1.");
+            Console.WriteLine("Площадь = " + SquareModel.SquareRectangle2(1.25 * Math.PI));
+
+            Console.WriteLine("perimetr - вещественное,меньше 1, равное 0.2. Площадь в сто раз меньше периметра, а также находится в периоде.");
+            Console.WriteLine("Площадь = " + SquareModel.SquareRectangle2(0.2));
+            Console.WriteLine();
         }
         static void RectangleGoldTest()
         {
-
+            Console.WriteLine("RectangleGoldTest");
         }
         static void TriangleTest()
         {
-
+            Console.WriteLine("TriangleTest");
         }
         static void HexagonTest()
         {
-
+            Console.WriteLine("HexagonTest");
         }
         static void RombTest()
         {
-
+            Console.WriteLine("RombTest");
         }
     }
 }
